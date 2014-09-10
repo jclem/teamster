a1b486b05c3052a03184818c6eb233f04528c00e
Jonathan Clem <jonathan@jclem.net>
2014-09-10 12:59:51 +0100

    add test for logger default to stdout

--- test/logger-test.js
+++ test/logger-test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var cluster = require('cluster');
+var sinon   = require('sinon');
 var through = require('through');
 var util    = require('util');
 
@@ -16,6 +17,36 @@ describe('logger', function() {
       logs.push(data);
     }, function end() {
       this.queue(null);
+    });
+  });
+
+  describe('when not given a verbose option', function() {
+    it('is verbose', function() {
+      require('../lib/logger')(undefined, stream).log({ foo: 'bar' });
+
+      logs.should.eql([
+        util.format('source=teamster:master pid=%d foo=bar\n', process.pid)
+      ]);
+    });
+  });
+
+  describe('when not given a stream', function() {
+    var outSpy;
+
+    beforeEach(function() {
+      outSpy = sinon.spy(process.stdout, 'write');
+    });
+
+    afterEach(function() {
+      process.stdout.write.restore();
+    });
+
+    it('logs to stdout', function() {
+      require('../lib/logger')().log({ foo: 'bar' });
+
+      outSpy.calledWithExactly(
+        util.format('source=teamster:master pid=%d foo=bar\n', process.pid)
+      ).should.eql(true);
     });
   });
 
